890fdd028f9c5490ebfde82dd2eb29407f7c3e89
ThomasEcalle <thomasecalle@hotmail.fr>
2019-08-21 10:52:59 +0200

    Refactor some code

--- lib/flappy_translator.dart
+++ lib/flappy_translator.dart
@@ -2,14 +2,15 @@ library flappy_translator;
 
 import 'dart:io';
 
-import 'csv_parser.dart';
-import 'file_parser.dart';
 import 'flappy_logger.dart';
+import 'parsing/csv_parser.dart';
+import 'parsing/file_parser.dart';
 
 const String VALUES_AREA_TEMPLATE_KEY = "/// Values area";
 const String FIELDS_AREA_TEMPLATE_KEY = "/// Fields area";
 const String SUPPORTED_LANGUAGES_AREA_TEMPLATE_KEY = "/// SupportedLanguages area";
 const String PARAMETERS_REGEX = r"\&([0-9])";
+const String TEMPLATE_FILE = "../lib/i18n.txt";
 
 class FlappyTranslator {
   void generate(String filePath) async {
@@ -20,7 +21,7 @@ class FlappyTranslator {
       return;
     }
 
-    final File templateFile = File("../lib/i18n.txt");
+    final File templateFile = File(TEMPLATE_FILE);
     String template = templateFile.readAsStringSync();
 
     final FileParser fileParser = CSVParser();
@@ -39,12 +40,13 @@ class FlappyTranslator {
       final String key = wordsOfLine.first;
       final words = wordsOfLine.sublist(1, wordsOfLine.length);
       final String defaultWord = words[0];
-      fields += _addField(key, defaultWord);
 
       if (defaultWord.isEmpty) {
         FlappyLogger.logError("$key has no translation for default language");
         return;
       }
+
+      fields += _addField(key, defaultWord);
 
       maps[0][key] = defaultWord;
       for (var wordIndex = 1; wordIndex < words.length; wordIndex++) {
@@ -55,11 +57,16 @@ class FlappyTranslator {
     template = template.replaceAll(FIELDS_AREA_TEMPLATE_KEY, fields);
     template = template.replaceAll(VALUES_AREA_TEMPLATE_KEY, _generateStringValuesFromList(maps, supportedLanguages));
 
+    _writeInFile(template);
+
+    FlappyLogger.logProgress("End of work !");
+  }
+
+  void _writeInFile(String template) {
     final File generatedFile = File("i18n.dart");
     generatedFile.createSync();
 
     generatedFile.writeAsStringSync(template, mode: FileMode.write);
-    FlappyLogger.logProgress("End of work !");
   }
 
   String _generateStringValuesFromList(List<Map<String, String>> maps, List<String> supportedLanguages) {
@@ -85,7 +92,7 @@ class FlappyTranslator {
 
       result += "};\n";
 
-      allValuesMap[lang] = maps[mapIndex];
+      allValuesMap[lang] = map;
     }
 
     result += """
@@ -100,7 +107,7 @@ class FlappyTranslator {
 
     result += "};";
 
-    return result.trim();
+    return result;
   }
 
   List<Map<String, String>> _generateValuesMaps(List<String> supportedLanguages) {
@@ -115,14 +122,12 @@ class FlappyTranslator {
     if (hasParameters) {
       String parameters = "";
       final List<RegExpMatch> matches = regex.allMatches(defaultWord).toList();
-      for (int index = 0; index < matches.length; index++) {
-        parameters += "String var${getParameterNameFromPlaceholder(matches[index].group(0))}, ";
+      for (RegExpMatch match in matches) {
+        parameters += "String var${getParameterNameFromPlaceholder(match.group(0))}, ";
       }
 
-      String result = "static String $key(BuildContext context, $parameters) {";
-
-      result += """
-      String text = _text("$key", context);\n
+      String result = """static String $key(BuildContext context, $parameters) {
+      String text = _text("$key", context);
       """;
 
       for (int index = 0; index < matches.length; index++) {
