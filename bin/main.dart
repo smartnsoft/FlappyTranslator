6001e51c192538c185272d4c7a1bade3ab02b8b1
ThomasEcalle <thomasecalle@hotmail.fr>
2019-08-21 11:04:51 +0200

    Change bin/main to handle target path

--- bin/main.dart
+++ bin/main.dart
@@ -3,10 +3,10 @@ import 'package:flappy_translator/flappy_translator.dart';
 
 void main(List<String> arguments) {
   if (arguments.isEmpty) {
-    FlappyLogger.logError("Missing arguments");
+    FlappyLogger.logError("Missing arguments (arguments are CSV file's name (mandatory) and target's file path)");
     return;
   }
 
   final flappyTranslator = FlappyTranslator();
-  flappyTranslator.generate(arguments.first);
+  flappyTranslator.generate(arguments.first, targetPath: arguments.length == 2 ? arguments[1] : null);
 }
