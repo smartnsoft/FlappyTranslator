b70f795fe1ca3b6135059e3be7f157f625d5f56d
James Leahy <defuncart@gmail.com>
2020-09-25 16:59:05 +0200

    Add ExcelParser.

---
+++ lib/parsing/excel_parser.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:excel/excel.dart';
+import 'package:meta/meta.dart';
+
+import 'file_parser.dart';
+
+/// An extension of [FileParser] for files of type xlsx
+class ExcelParser extends FileParser {
+  ExcelParser({
+    @required File file,
+    @required int startIndex,
+  }) : super(file: file, startIndex: startIndex);
+
+  @override
+  void parseFile() {
+    final bytes = file.readAsBytesSync();
+    final excel = Excel.decodeBytes(bytes);
+    final table = excel.tables?.keys?.first;
+
+    for (final row in excel.tables[table].rows) {
+      final rowAsStrings = row.map((element) => element.toString()).toList();
+      parsedContents.add(rowAsStrings);
+    }
+  }
+}
