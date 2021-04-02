95d32c31fbc4a006b8be07ba10db0ce31a927430
James Leahy <defuncart@gmail.com>
2021-04-02 10:30:35 +0200

    Ensure all non-public facing code is private.

--- bin/flappy_translator.dart
+++ bin/flappy_translator.dart
@@ -3,14 +3,49 @@ import 'dart:io';
 import 'package:flappy_translator/flappy_translator.dart';
 import 'package:yaml/yaml.dart';
 
+void main() {
+  String? inputFilePath;
+
+  // try to load settings from the project's pubspec.yaml
+  final settings = _loadSettings();
+  if (settings.isNotEmpty) {
+    if (settings.containsKey(_YamlArguments.inputFilePath)) {
+      inputFilePath = settings[_YamlArguments.inputFilePath];
+    }
+  }
+
+  // display an error and quit if the input file hasn't been specified
+  if ((inputFilePath == null)) {
+    print('[ERROR] Input file path not defined. This can be set as a command line argument or in pubspec.yaml\n');
+    return;
+  }
+
+  // parse csv to dart
+  final flappyTranslator = FlappyTranslator();
+  flappyTranslator.generate(
+    inputFilePath,
+    outputDir: settings[_YamlArguments.outputDir],
+    fileName: settings[_YamlArguments.fileName],
+    className: settings[_YamlArguments.className],
+    delimiter: settings[_YamlArguments.delimiter],
+    startIndex: settings[_YamlArguments.startIndex],
+    dependOnContext: settings[_YamlArguments.dependOnContext],
+    useSingleQuotes: settings[_YamlArguments.useSingleQuotes],
+    replaceNoBreakSpaces: settings[_YamlArguments.replaceNoBreakSpaces],
+    exposeGetString: settings[_YamlArguments.exposeGetString],
+    exposeLocaStrings: settings[_YamlArguments.exposeLocaStrings],
+    exposeLocaleMaps: settings[_YamlArguments.exposeLocaleMaps],
+  );
+}
+
 /// The path to the pubspec file path
-const pubspecFilePath = 'pubspec.yaml';
+const _pubspecFilePath = 'pubspec.yaml';
 
 /// The section id for flappy-translator in the yaml file
-const yamlSectionId = 'flappy_translator';
+const _yamlSectionId = 'flappy_translator';
 
 /// A class of arguments which the user can specify in pubspec.yaml
-class YamlArguments {
+class _YamlArguments {
   static const inputFilePath = 'input_file_path';
   static const outputDir = 'output_dir';
   static const className = 'class_name';
@@ -25,51 +60,16 @@ class YamlArguments {
   static const exposeLocaleMaps = 'expose_locale_maps';
 }
 
-void main() {
-  String? inputFilePath;
-
-  // try to load settings from the project's pubspec.yaml
-  final settings = loadSettings();
-  if (settings.isNotEmpty) {
-    if (settings.containsKey(YamlArguments.inputFilePath)) {
-      inputFilePath = settings[YamlArguments.inputFilePath];
-    }
-  }
-
-  // display an error and quit if the input file hasn't been specified
-  if ((inputFilePath == null)) {
-    print('[ERROR] Input file path not defined. This can be set as a command line argument or in pubspec.yaml\n');
-    return;
-  }
-
-  // parse csv to dart
-  final flappyTranslator = FlappyTranslator();
-  flappyTranslator.generate(
-    inputFilePath,
-    outputDir: settings[YamlArguments.outputDir],
-    fileName: settings[YamlArguments.fileName],
-    className: settings[YamlArguments.className],
-    delimiter: settings[YamlArguments.delimiter],
-    startIndex: settings[YamlArguments.startIndex],
-    dependOnContext: settings[YamlArguments.dependOnContext],
-    useSingleQuotes: settings[YamlArguments.useSingleQuotes],
-    replaceNoBreakSpaces: settings[YamlArguments.replaceNoBreakSpaces],
-    exposeGetString: settings[YamlArguments.exposeGetString],
-    exposeLocaStrings: settings[YamlArguments.exposeLocaStrings],
-    exposeLocaleMaps: settings[YamlArguments.exposeLocaleMaps],
-  );
-}
-
 /// Returns configuration settings for flappy_translator from pubspec.yaml
-Map<String?, dynamic> loadSettings() {
-  final file = File(pubspecFilePath);
+Map<String?, dynamic> _loadSettings() {
+  final file = File(_pubspecFilePath);
   final yamlString = file.readAsStringSync();
   final Map<dynamic, dynamic> yamlMap = loadYaml(yamlString);
 
   // determine <String?, dynamic> map from <dynamic, dynamic> yaml
   final settings = <String?, dynamic>{};
-  if (yamlMap.containsKey(yamlSectionId)) {
-    for (final kvp in yamlMap[yamlSectionId].entries) {
+  if (yamlMap.containsKey(_yamlSectionId)) {
+    for (final kvp in yamlMap[_yamlSectionId].entries) {
       settings[kvp.key] = kvp.value;
     }
   }
